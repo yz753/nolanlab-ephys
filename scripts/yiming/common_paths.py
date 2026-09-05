ee8d1f3d5495a1a16aec520fab9c7e816d2d19ea
Yiming <yimingzhao1219@gmail.com>
2026-09-05 01:01:51 +0100

    Fix bugs

--- scripts/yiming/common_paths.py
+++ scripts/yiming/common_paths.py
@@ -6,6 +6,6 @@ eddie_yiming_data_folder = root / 'spikesorting/raw'
 
 eddie_yiming_deriv_folder = root / 'spikesorting/derivatives'
 
-eddie_yiming_csv_path = root / 'spikesorting/yiming_filepaths.csv'
-
 eddie_active_projects = Path("/exports/cmvm/datastore/sbms/groups/INCR-NolanLab/ActiveProjects")
+
+eddie_yiming_csv_path = root / 'nolanlab-ephys/scripts/yiming/yiming_filepaths.csv'
